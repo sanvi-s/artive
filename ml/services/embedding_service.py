abbe6f4b54d688bfdda8686594e26dc18e5b5692
sanvi-s <shanishcharasanvi@gmail.com>
2026-06-20 00:52:16 +0530

    ML service

--- ml/services/embedding_service.py
+++ ml/services/embedding_service.py
@@ -1,3 +1,15 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+_model = None
+
+def get_model():
+    global _model
+    if _model is None:
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model
+
+class _LazyModel:
+    def encode(self, *args, **kwargs):
+        return get_model().encode(*args, **kwargs)
+
+model = _LazyModel()
